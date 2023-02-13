734a76d1c1d5c76d105965b16f9f27b520645011
basudevnayak <basudevnayak31@gmail.com>
2023-02-13 22:54:52 +0530

    update file

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Icon name="search" size={22} color="#000" />
+    </View>
+  );
+}
+
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article Screen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
---
+++ src/color/Color.tsx
@@ -0,0 +1,14 @@
+const Color = {
+    white: '#FFF',
+    dark: '#000',
+    primary: '#FFA500',
+    secondary: '#fedac5',
+    light: '#E5E5E5',
+    grey: '#908e8c',
+    green_light:'#00C9A7',
+    Navy  :'#000080',
+    blue:"#2874f0",
+  };
+  
+  export default Color;
+ 
